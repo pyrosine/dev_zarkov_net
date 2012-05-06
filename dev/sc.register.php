6f3b54bfeabfb72f35930555b7b34668574d201c
pyrosine <pyrosine@gmail.com>
2012-05-06 17:16:38 +0100

    a024

--- dev/sc.register.php
+++ dev/sc.register.php
@@ -12,7 +12,7 @@ $eq=mysql_query("select * from devs where email='".mysql_real_escape_string($e).
 if(mysql_num_rows($eq)!=0) header("location:register.php?e=eexists");
 
 
-mysql_query("insert into devs (user,pass,email) values ('".mysql_real_escape_string($u)."','".sha1($p."strawberries")."','".mysql_real_escape_string($e)."')") or die("Failed to add user");
+mysql_query("insert into devs (user,pass,email) values ('".mysql_real_escape_string($u)."','".sha1($p."strawberries")."','".mysql_real_escape_string($e)."')") or die(mysql_error());
 $did=mysql_insert_id();
 $expiry=time()+(3600*24);
 $token=sha1(time()."pyrosine");
