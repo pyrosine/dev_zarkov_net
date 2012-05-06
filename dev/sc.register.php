665adbf54b4894d21a66d01b5008079d5db9ebb1
pyrosine <pyrosine@gmail.com>
2012-05-06 17:12:06 +0100

    a023

--- dev/sc.register.php
+++ dev/sc.register.php
@@ -12,14 +12,14 @@ $eq=mysql_query("select * from devs where email='".mysql_real_escape_string($e).
 if(mysql_num_rows($eq)!=0) header("location:register.php?e=eexists");
 
 
-mysql_query("insert into devs (user,pass,email) values ('".mysql_real_escape_string($u)."','".sha1($p."strawberries")."','".mysql_real_escape_string($e)."')");
+mysql_query("insert into devs (user,pass,email) values ('".mysql_real_escape_string($u)."','".sha1($p."strawberries")."','".mysql_real_escape_string($e)."')") or die("Failed to add user");
 $did=mysql_insert_id();
 $expiry=time()+(3600*24);
 $token=sha1(time()."pyrosine");
 
 mysql_query("insert into dev_sessions (did,iid,fid,timestamp,token,expiry) values ($did,$iid,$fid,".time().",'$token',$expiry)");
-setcookie('dev_id','$did',$expiry,'/','dev.zarkov.net');
-setcookie('dev_token','$token',$expiry,'/','dev.zarkov.net');
+setcookie('dev_id',$did,$expiry,'/','dev.zarkov.net');
+setcookie('dev_token',$token,$expiry,'/','dev.zarkov.net');
 
 header("index.php");
 
