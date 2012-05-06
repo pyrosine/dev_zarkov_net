e4f0e4a15857976607adb77ed53960dd97f27d82
pyrosine <pyrosine@gmail.com>
2012-05-06 17:42:15 +0100

    a032

---
+++ dev/inc.menu.php
@@ -0,0 +1,9 @@
+<?php
+echo("<div style='width: 100%; height: 50px; background-color: #D4D4D4;'>");
+	item("Home","../index.php","left");
+	item("Developer Index","index.php","left");
+	item("Search projects","search.php","left");
+	item("Logout","sc.logout.php","right");
+echo("</div>");
+
+?>
---
+++ manager/inc.menu.php
@@ -0,0 +1,10 @@
+<?php
+echo("<div style='width: 100%; height: 50px; background-color: #D4D4D4;'>");
+	item("Home","../index.php","left");
+	item("Manager Index","index.php","left");
+	item("Create project","create.php","left");
+	item("Search previous projects","search.php","left");
+	item("Logout","sc.logout.php","right");
+echo("</div>");
+
+?>
