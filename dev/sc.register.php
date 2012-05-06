4b6be3a05744b043e5b68ceacc0579f447d00d50
pyrosine <pyrosine@gmail.com>
2012-05-06 16:55:49 +0100

    a021

--- dev/sc.register.php
+++ dev/sc.register.php
@@ -16,7 +16,7 @@ mysql_query("insert into devs (user,pass,email) values ('".mysql_real_escape_str
 $did=mysql_insert_id();
 $expiry=time()+(3600*24);
 $token=sha1(time()."pyrosine");
-	
+
 mysql_query("insert into dev_sessions (did,iid,fid,timestamp,token,expiry) values ($did,$iid,$fid,".time().",'$token',$expiry)");
 setcookie('dev_id','$did',$expiry,'/','dev.zarkov.net');
 setcookie('dev_token','$token',$expiry,'/','dev.zarkov.net');
