d4872e3a2f3cf82388a3b80e8789ef92d1fa89b2
pyrosine <pyrosine@gmail.com>
2012-05-06 17:27:44 +0100

    a028

--- dev/inc.user.php
+++ dev/inc.user.php
@@ -6,7 +6,7 @@ $token=$_COOKIE["dev_token"];
 
 if($uid==null || $token==null) {$e="nocookies"; include("inc.login.php");}
 
-$v=mysql_query("select * from dev_sessions where expired=0 and id=$uid and token='$token' and fid=$fid");
+$v=mysql_query("select * from dev_sessions where expired=0 and did=$uid and token='$token' and fid=$fid");
 if(mysql_num_rows($v)==0) {$e="nosession"; include("inc.login.php");}
 
 $did=mysql_result($v,0,did);
--- manager/inc.user.php
+++ manager/inc.user.php
@@ -6,7 +6,7 @@ $token=$_COOKIE["man_token"];
 
 if($uid==null || $token==null) {$e="nocookies"; include("inc.login.php");}
 
-$v=mysql_query("select * from manager_sessions where expired=0 and id=$uid and token='$token' and fid=$fid");
+$v=mysql_query("select * from manager_sessions where expired=0 and mid=$uid and token='$token' and fid=$fid");
 if(mysql_num_rows($v)==0) {$e="nosession"; include("inc.login.php");}
 
 $mid=mysql_result($v,0,mid);
