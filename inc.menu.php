61cf8ac9361d05e010889246b3bd4513cf6400ab
pyrosine <pyrosine@gmail.com>
2012-05-06 14:42:55 +0100

    a006

--- inc.menu.php
+++ inc.menu.php
@@ -1,9 +1,10 @@
 <?php
 $m_item=0;
 function item($item,$url){
-	echo("<div id='$m_item' style='background-color: gray; display: inline; border:1px dotted;' ");
-		echo("onMouseOver='document.getElementById($m_item).style.background-color=".'"red"'."' ");
-		echo("onMouseOut='document.getElementById($m_item).style.background-color=".'"gray"'."' ");
+	global $m_item;
+	echo("<div id='$m_item' style='background-color: #8C8C8C; display: inline; border:1px dotted;' ");
+		echo("onMouseOver='document.getElementById($m_item).style.background-color=".'"#A8A8A8"'."' ");
+		echo("onMouseOut='document.getElementById($m_item).style.background-color=".'"#8C8C8C"'."' ");
 		echo(">");
 		echo("<a href='$url'>$item</a>");
 	echo("</div>");
@@ -11,7 +12,8 @@ function item($item,$url){
 }
 echo("<div style='width: 90%; margin-right: auto; margin-left: auto; height: 40px;'>");
 	item("Home","index.php");
-	item("Projects","p.php");
+	item("For Developers","dev/index.php");
+	item("For Managers","manager/index.php");
 echo("</div>");
 
 ?>
