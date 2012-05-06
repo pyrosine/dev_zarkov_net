474999c6b0a5bee72a82ffa1a365b26a2f0af06a
pyrosine <pyrosine@gmail.com>
2012-05-06 17:32:16 +0100

    a030

--- dev/index.php
+++ dev/index.php
@@ -8,11 +8,11 @@ $q=mysql_query("select project_devs.*,projects.* where project_devs.did=$did and
 for($z=0;$z<mysql_num_rows($q);$z++){
 	echo("<tr><td>".mysql_result($q,$z,projects.title)."</td><td>".mysql_result($q,$z,project_devs.role)."</td><td>".date('dS m y',mysql_result($q,$z,project_devs.ETA))."</td></tr>");
 }
-echo("</div><hr>");
+echo("</table></div><hr>");
 
 echo("<h2>Open Projects:</h2>"."<div><table><tr><th>Project title</th><th>Date created</th><th>Deadline for applications</th><th>To be finished before</th></tr>");
 $q2=mysql_query("select * from projects where active=1");
 for($z=0;$z<mysql_num_rows($q2);$z++){
 	echo("<tr><td>".mysql_result($q2,$z,title)."</td><td>".date('dS m y',mysql_result($q,$z,timestamp))."</td><td>".date('dS m y',mysql_result($q,$z,deadline_application))."</td><td>".date('dS m y',mysql_result($q,$z,deadline))."</td></tr>");
 }
-echo("</div><hr>");
+echo("</table></div><hr>");
