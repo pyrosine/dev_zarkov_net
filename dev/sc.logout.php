1e3acc489a5ef2acbaf285a3a55dadc40ef091ee
pyrosine <pyrosine@gmail.com>
2012-05-07 12:58:52 +0100

    a038

--- dev/sc.logout.php
+++ dev/sc.logout.php
@@ -2,7 +2,7 @@
 
 include("../inc/track.php");
 $token=$_COOKIE["dev_token"];
-mysql_query("update dev_sessions set expired=1,expiry=".time()." where token='".mysq_real_escape_string($token)."'");
+mysql_query("update dev_sessions set expired=1,expiry=".time()." where token='".mysql_real_escape_string($token)."'");
 
 setcookie("dev_id","",time()-360000,"/","dev.zarkov.net");
 setcookie("dev_token","",time()-360000,"/","dev.zarkov.net");
--- manager/sc.logout.php
+++ manager/sc.logout.php
@@ -2,7 +2,7 @@
 
 include("../inc/track.php");
 $token=$_COOKIE["man_token"];
-mysql_query("update manager_sessions set expired=1,expiry=".time()." where token='".mysq_real_escape_string($token)."'");
+mysql_query("update manager_sessions set expired=1,expiry=".time()." where token='".mysql_real_escape_string($token)."'");
 
 setcookie("man_id","",time()-360000,"/","dev.zarkov.net");
 setcookie("man_token","",time()-360000,"/","dev.zarkov.net");
