a99db75d08df0737c6860993af05ac16fa929816
pyrosine <pyrosine@gmail.com>
2012-05-07 12:56:52 +0100

    a037 +logout

--- dev/sc.logout.php
+++ dev/sc.logout.php
@@ -4,6 +4,9 @@ include("../inc/track.php");
 $token=$_COOKIE["dev_token"];
 mysql_query("update dev_sessions set expired=1,expiry=".time()." where token='".mysq_real_escape_string($token)."'");
 
-header("../index.php?o=success");
+setcookie("dev_id","",time()-360000,"/","dev.zarkov.net");
+setcookie("dev_token","",time()-360000,"/","dev.zarkov.net");
+
+header("location:../index.php?o=success");
 
 ?>
---
+++ manager/sc.logout.php
@@ -0,0 +1,12 @@
+<?php
+
+include("../inc/track.php");
+$token=$_COOKIE["man_token"];
+mysql_query("update manager_sessions set expired=1,expiry=".time()." where token='".mysq_real_escape_string($token)."'");
+
+setcookie("man_id","",time()-360000,"/","dev.zarkov.net");
+setcookie("man_token","",time()-360000,"/","dev.zarkov.net");
+
+header("location:../index.php?o=success");
+
+?>
